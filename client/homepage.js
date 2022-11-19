68367ca5f0f78ba289e2aeb3f2bb227c8b6dfe0a
EveRoutledge <eve.routledge@gmail.com>
2022-11-19 23:54:09 +0000

    dataset map

--- client/homepage.js
+++ client/homepage.js
@@ -1,7 +1,18 @@
 let map, infoWindow;
-let dataset = [];
+let dataset = JSON.parse(fs.readFileSync('../Charities.json'))['Charities'];
+let hours, type, postcode;
+
 
 function initMap() {
+    var svgMarker = {
+        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+        fillColor: "blue",
+        fillOpacity: 0.6,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(15, 30),
+      };
     map = new google.maps.Map(document.getElementById('map'),{
         center: location,
         zoom: 15 // city level
@@ -24,9 +35,19 @@ function initMap() {
             handleLocationError(true, infoWindow, map.getCenter());
           }
         );
-        for (let i=0; i <dataset.length;i++){
+        for (let i=0; i < dataset.length;i++){
+            if (dataset[i]['Volunteering Hours Per Week']>10){
+                svgMarker.fillColor = 'red'
+            } else if (dataset[i]['Volunteering Hours Per Week']>5){
+                svgMarker.fillColor='orange'
+            } else if (dataset[i]['Volunteering Hours Per Week']>2) {
+                svgMarker.fillColor = 'green'
+            } else {
+                svgMarker.fillColor = 'grey'
+            }
             const marker = new google.maps.Marker({
-                position: {lat:dataset[i].lat, lng:dataset[i].lng},
+                position: {lat:dataset[i]['Latitude'], lng:dataset[i]['Latitude']},
+                icon: svgMarker,
                 map: map,
               });
         }
@@ -47,6 +68,12 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
   }
 
-
-
 window.initMap=initMap;
+
+document.getElementById('search-btn').addEventListener('click', () => {
+    var select1 = document.getElementById('HourSelect');
+    var select2 = document.getElementById('TypeSelect');
+    postcode = document.getElementById('postcode');
+    hours = select1.options[select1.selectedIndex].text;
+    type = select2.option[select2.selectedIndex].text;
+});
