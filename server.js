79043cb9b89dc98397bb773aa4eb017fc12cb044
EveRoutledge <eve.routledge@gmail.com>
2022-11-20 10:34:08 +0000

    It can get entities now!

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const app = require('./app');
 
-app.listen(8090);
+const PORT = process.env.PORT || 8090;
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}: http://localhost:${PORT}`);
+});
