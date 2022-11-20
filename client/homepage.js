d7a49ba4699a91e9545b05fe2ed24642411567c7
EveRoutledge <eve.routledge@gmail.com>
2022-11-20 11:22:19 +0000

    coloured markers

--- client/homepage.js
+++ client/homepage.js
@@ -1,13 +1,11 @@
 let map, infoWindow;
-let marker;
-let dataset;
-let hours, type, postcode;
-
+let hours,postcode,type = null;
 
 async function initMap() {
     // load in dataset of charities
-    const charities = await fetch("/charities").then(res => res.json());
-    console.log(charities)
+    const marker = [];
+    const dataset = await fetch("/charities").then(res => res.json());
+    console.log(dataset.length);
     var svgMarker = {
         path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
         fillColor: "blue",
@@ -39,6 +37,7 @@ async function initMap() {
             handleLocationError(true, infoWindow, map.getCenter());
           }
         );
+        
         for (let i=0; i < dataset.length;i++){
             if (hours !== null){
                 if (dataset[i].volHoursPerWeek<hours){
@@ -59,8 +58,9 @@ async function initMap() {
             } else {
                 svgMarker.fillColor = 'grey';
             }
-            marker.push(new google.maps.Marker({
-                position: {lat:dataset[i].latitude, lng:dataset[i].latitude},
+            console.log(dataset[i].longitude)
+           marker.push(new google.maps.Marker({
+                position: {lat:dataset[i].latitude, lng:dataset[i].longitude},
                 icon: svgMarker,
                 map: map,
               }));
@@ -69,7 +69,7 @@ async function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
       }
-
+      console.log(marker)
 };
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
